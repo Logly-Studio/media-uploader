e2b29ce31a6afc46e2aa7968cddbf0654d9244ae
Bryant Young <bryantryoung19@gmail.com>
2023-06-30 16:39:47 -0500

    successful build

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 import { resolve } from "path";
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     lib: {
-      entry: resolve(__dirname, "./main.tsx"),
+      entry: resolve(__dirname, "./src/main.tsx"),
       name: "@logly/media-uploads",
       fileName: "media-uploads",
     },
